9c13317a8c7e724de3b0e0ff291ead588951ec6b
Francisco Ariel Berrones Gómez <francisco.berrones@udem.edu>
2025-01-31 15:01:12 -0600

    Update test-linear-regression.py

--- test-linear-regression.py
+++ test-linear-regression.py
@@ -4,3 +4,34 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import time
+
+# 1. Seed for reproducibility
+np.random.seed(4500)
+
+# 2. Generate synthetic data
+x = np.arange(0,100, 1)
+noise_mean, noise_std = 0, 5
+true_coeffs = [1, 3]
+y = true_coeffs[1]*x + true_coeffs[0] + np.random.normal(loc=noise_mean, scale=noise_std, size=len(x))
+
+# 3. Visualise the generated synthetic dataset
+plt.figure(figsize=(10,7))
+plt.scatter(x, y, label='Synthetic dataset')
+plt.xlabel(r"$x$", fontsize=20)
+plt.ylabel("$f_{\mathbf{w}}(x)$", fontsize=20)
+plt.title(rf"$f_{{\mathbf{{w}}}}(x) = {true_coeffs[1]} x + {true_coeffs[0]} + \epsilon$, where $\epsilon \sim \mathcal{{N}}(\mu=0, \sigma={noise_std})$", fontsize=20)
+plt.legend()
+plt.show()
+
+# 4. Split the data into training and testing sets using train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42, shuffle=True)
+
+# 5. Visualise the generated train and test synthetic dat sets
+plt.figure(figsize=(10,7))
+plt.scatter(x, y, label='Testing dataset')
+plt.scatter(x_train, y_train, label='Training dataset', color=[1,0,0])
+plt.xlabel(r"$x$", fontsize=20)
+plt.ylabel("$f_{\mathbf{w}}(x)$", fontsize=20)
+plt.title(rf"$f_{{\mathbf{{w}}}}(x) = {true_coeffs[1]} x + {true_coeffs[0]} + \epsilon$, where $\epsilon \sim \mathcal{{N}}(\mu=0, \sigma={noise_std})$", fontsize=20)
+plt.legend()
+plt.show()
